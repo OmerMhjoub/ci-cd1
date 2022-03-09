3cfc6b6d6cc553f89f64652a0fd6698583469eaa
OmerMhjoub <99316771+OmerMhjoub@users.noreply.github.com>
2022-03-09 13:09:00 +0300

    fixed

--- src/factorial.py
+++ src/factorial.py
@@ -1,4 +1,12 @@
+"""
+This module has the method to calculate factorial of the given number
+"""
+
 def factorial(n):
+    """ Calculates factorial of the passed number
+    num (int): Integer for which factorial is requested
+    returns (int): Returns Factorial
+    """
     if n == 0 or n == 1:
         return 1
     return n * factorial(n-1)
